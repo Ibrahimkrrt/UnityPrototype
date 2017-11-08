54ea886454175744713aa7150ff84d863eaa52ee
ibrahim <ibrahim.krrt@gmail.com>
2017-11-08 22:03:16 +0100

    first commit

---
+++ VR Project/Assets/CubeJ.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeJ : MonoBehaviour {
+
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float gravity = 20.0f;
+    private float jumForce = 8.0f;
+    // Use this for initialization
+    void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
---
+++ VR Project/Assets/CubeM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeM : MonoBehaviour {
+
+    private float MovmentSpeed=2.0f;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            MovmentSpeed = 4.0f;
+        }
+        else
+        {
+            MovmentSpeed = 2.0f;
+        }
+        transform.Translate(MovmentSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, MovmentSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coins"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
